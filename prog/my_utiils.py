664ec1b9ad7bb5bb12c76d31538b956c4db3bf6e
LX <51038252+liuxuan666@users.noreply.github.com>
2021-06-03 14:55:23 +0800

    Add files via upload

--- prog/my_utiils.py
+++ prog/my_utiils.py
@@ -43,14 +43,33 @@ def metrics_graph(yt, yp):
     precision, recall, _, = precision_recall_curve(yt, yp)
     aupr = -np.trapz(precision, recall)
     auc = roc_auc_score(yt, yp)
-    #---f1-acc
-    tp = (yt * yp).sum()
-    tn = ((1 - yt) * (1 - yp)).sum()
-    fp = ((1 - yt) * yp).sum()
-    fn = (yt * (1 - yp)).sum()
-    epsilon = 0.0000001
-    accuracy= (tp + tn) / (tp + tn + fn + fp)
-    precision2 = tp / (tp + fp + epsilon)
-    recall2 = tp / (tp + fn + epsilon)
-    f1 = 2 * (precision2 * recall2) / (precision2 + recall2 + epsilon)
-    return auc,aupr,f1,accuracy,precision2,recall2
+    #---f1,acc,recall, specificity, precision
+    real_score=np.mat(yt)
+    predict_score=np.mat(yp)
+    sorted_predict_score = np.array(sorted(list(set(np.array(predict_score).flatten()))))
+    sorted_predict_score_num = len(sorted_predict_score)
+    thresholds = sorted_predict_score[np.int32(sorted_predict_score_num * np.arange(1, 1000) / 1000)]
+    thresholds = np.mat(thresholds)
+    thresholds_num = thresholds.shape[1]
+    predict_score_matrix = np.tile(predict_score, (thresholds_num, 1))
+    negative_index = np.where(predict_score_matrix < thresholds.T)
+    positive_index = np.where(predict_score_matrix >= thresholds.T)
+    predict_score_matrix[negative_index] = 0
+    predict_score_matrix[positive_index] = 1
+    TP = predict_score_matrix.dot(real_score.T)
+    FP = predict_score_matrix.sum(axis=1) - TP
+    FN = real_score.sum() - TP
+    TN = len(real_score.T) - TP - FP - FN
+    tpr = TP / (TP + FN)
+    recall_list = tpr
+    precision_list = TP / (TP + FP)
+    f1_score_list = 2 * TP / (len(real_score.T) + TP - TN)
+    accuracy_list = (TP + TN) / len(real_score.T)
+    specificity_list = TN / (TN + FP)
+    max_index = np.argmax(f1_score_list)
+    f1_score = f1_score_list[max_index]
+    accuracy = accuracy_list[max_index]
+    specificity = specificity_list[max_index]
+    recall = recall_list[max_index]
+    precision = precision_list[max_index]
+    return auc, aupr, f1_score[0, 0], accuracy[0, 0] #, recall[0, 0], specificity[0, 0], precision[0, 0]
