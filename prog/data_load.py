8424a57e58e804456a48890f91d8caf4b3c59cc1
LX <51038252+liuxuan666@users.noreply.github.com>
2021-07-25 11:35:56 +0800

    Update data_load.py

--- prog/data_load.py
+++ prog/data_load.py
@@ -12,16 +12,16 @@ def dataload(Drug_info_file, IC50_threds_file, Drug_feature_file, Cell_line_info
     drug2thred={}
     for line in open(IC50_threds_file).readlines()[1:]:
         drug2thred[str(line.split('\t')[0])]=float(line.strip().split('\t')[1])
-    '''
-    IC50threds = pd.read_csv(IC50_threds_file, sep=',',header=0)
-    drug2dict  = dict(zip(IC50threds['pubchem'],IC50threds['IC50']))
-    IC50key=[]; IC50value=[]
-    for key, value in drug2dict.items():
-        key=str(key)
-        IC50key.append(key)
-        IC50value.append(value)
-    drug2thred = dict(zip(IC50key,IC50value))
-    '''
+#     '''
+#     IC50threds = pd.read_csv(IC50_threds_file, sep=',',header=0)
+#     drug2dict  = dict(zip(IC50threds['pubchem'],IC50threds['IC50']))
+#     IC50key=[]; IC50value=[]
+#     for key, value in drug2dict.items():
+#         key=str(key)
+#         IC50key.append(key)
+#         IC50value.append(value)
+#     drug2thred = dict(zip(IC50key,IC50value))
+#     '''
     drug_pubchem_id_set = []
     drug_feature = {}
     for each in os.listdir(Drug_feature_file):
