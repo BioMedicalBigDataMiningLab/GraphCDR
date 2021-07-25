77c3cd6f5e3e4d817008a55d4e3a27387cbcbb28
LX <51038252+liuxuan666@users.noreply.github.com>
2021-07-25 11:19:34 +0800

    Update SGConv.py

--- prog/base_model/SGConv.py
+++ prog/base_model/SGConv.py
@@ -9,38 +9,7 @@ from torch_geometric.nn.conv.gcn_conv import gcn_norm
 
 
 class SGConv(MessagePassing):
-    r"""The simple graph convolutional operator from the `"Simplifying Graph
-    Convolutional Networks" <https://arxiv.org/abs/1902.07153>`_ paper
-
-    .. math::
-        \mathbf{X}^{\prime} = {\left(\mathbf{\hat{D}}^{-1/2} \mathbf{\hat{A}}
-        \mathbf{\hat{D}}^{-1/2} \right)}^K \mathbf{X} \mathbf{\Theta},
-
-    where :math:`\mathbf{\hat{A}} = \mathbf{A} + \mathbf{I}` denotes the
-    adjacency matrix with inserted self-loops and
-    :math:`\hat{D}_{ii} = \sum_{j=0} \hat{A}_{ij}` its diagonal degree matrix.
-    The adjacency matrix can include other values than :obj:`1` representing
-    edge weights via the optional :obj:`edge_weight` tensor.
-
-    Args:
-        in_channels (int): Size of each input sample.
-        out_channels (int): Size of each output sample.
-        K (int, optional): Number of hops :math:`K`. (default: :obj:`1`)
-        cached (bool, optional): If set to :obj:`True`, the layer will cache
-            the computation of :math:`{\left(\mathbf{\hat{D}}^{-1/2}
-            \mathbf{\hat{A}} \mathbf{\hat{D}}^{-1/2} \right)}^K \mathbf{X}` on
-            first execution, and will use the cached version for further
-            executions.
-            This parameter should only be set to :obj:`True` in transductive
-            learning scenarios. (default: :obj:`False`)
-        add_self_loops (bool, optional): If set to :obj:`False`, will not add
-            self-loops to the input graph. (default: :obj:`True`)
-        bias (bool, optional): If set to :obj:`False`, the layer will not learn
-            an additive bias. (default: :obj:`True`)
-        **kwargs (optional): Additional arguments of
-            :class:`torch_geometric.nn.conv.MessagePassing`.
-    """
-
+    
     _cached_x: Optional[Tensor]
 
     def __init__(self, in_channels: int, out_channels: int, K: int = 1,
